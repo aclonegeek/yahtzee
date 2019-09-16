59df902035dbaed909ee37b6675d688293a867e0
Randy Taylor <tehgecKozzz@gmail.com>
2019-09-15 22:45:48 -0400

    code: Store each client that connects in the Server class

--- src/main/java/com/yahtzee/core/Server.java
+++ src/main/java/com/yahtzee/core/Server.java
@@ -4,9 +4,12 @@ package com.yahtzee.core;
 
 import java.io.IOException;
 import java.net.ServerSocket;
+import java.util.ArrayList;
 
 public class Server {
-    public static void main(String[] args) throws IOException {
+    private ArrayList<ServerThread> clients;
+
+    public static void main(String[] args) {
         if (args.length != 1) {
             System.err.println("Specify a port number");
             System.exit(-1);
@@ -14,11 +17,22 @@ public class Server {
 
         int port = Integer.parseInt(args[0]);
 
+        Server server = new Server();
+        server.start(port);
+    }
+
+    public Server() {
+        this.clients = new ArrayList<>();
+    }
+
+    public void start(int port) {
         System.out.println("Waiting for players to join...");
 
         try (ServerSocket serverSocket = new ServerSocket(port)) {
             while (true) {
-                new ServerThread(serverSocket.accept()).start();
+                ServerThread serverThread = new ServerThread(serverSocket.accept());
+                this.clients.add(serverThread);
+                serverThread.start();
             }
         } catch (IOException e) {
             System.err.println("Could not listen on port " + port);
