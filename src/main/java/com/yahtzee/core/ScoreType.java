cd73569e17027b9338afdd4dee2a68c43ab044aa
Randy Taylor <tehgecKozzz@gmail.com>
2019-09-23 16:24:53 -0400

    code: Add ScoreType enum

---
+++ src/main/java/com/yahtzee/core/ScoreType.java
@@ -0,0 +1,29 @@
+package com.yahtzee.core;
+
+public enum ScoreType {
+    ONES("Ones"),
+    TWOS("Twos"),
+    THREES("Threes"),
+    FOURS("Fours"),
+    FIVES("Fives"),
+    SIXES("Sixes"),
+    LARGE_STRAIGHT("Large straight"),
+    SMALL_STRAIGHT("Small straight"),
+    FULL_HOUSE("Full House"),
+    THREE_OF_A_KIND("Three of a Kind"),
+    FOUR_OF_A_KIND("Four of a Kind"),
+    CHANCE("Chance"),
+    YAHTZEE("Yahtzee"),
+    BONUS("Bonus");
+
+    private final String name;
+
+    private ScoreType(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return this.name;
+    }
+}
