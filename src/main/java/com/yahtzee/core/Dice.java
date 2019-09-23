69ac9a55c4b399866b856b17a7a15769e6e77379
Randy Taylor <tehgecKozzz@gmail.com>
2019-09-23 16:24:03 -0400

    code: Add Dice.setValue and a ctor that takes a value

--- src/main/java/com/yahtzee/core/Dice.java
+++ src/main/java/com/yahtzee/core/Dice.java
@@ -3,8 +3,17 @@ package com.yahtzee.core;
 public class Dice {
     private int value;
 
+    Dice() {}
+    Dice(int value) {
+        this.value = value;
+    }
+
     public void roll() {
         this.value = (int)((Math.random() * 6) + 1);
+    }
+
+    public void setValue(int value) {
+        this.value = value;
     }
 
     public int getValue() {
