60174226ccd272db7b250b0a5842cb793ef8a339
Randy Taylor <tehgecKozzz@gmail.com>
2019-09-23 16:27:00 -0400

    code: Add ScoreSheet class and implement scoring the upper section without bonuses (yet). Also implement calculating the score of a score sheet

---
+++ src/main/java/com/yahtzee/core/ScoreSheet.java
@@ -0,0 +1,63 @@
+package com.yahtzee.core;
+
+import java.util.HashMap;
+
+public class ScoreSheet {
+    private HashMap<ScoreType, Integer> scoreSheet;
+
+    public ScoreSheet() {
+        this.scoreSheet = new HashMap<>();
+        this.scoreSheet.put(ScoreType.ONES, 0);
+        this.scoreSheet.put(ScoreType.TWOS, 0);
+        this.scoreSheet.put(ScoreType.THREES, 0);
+        this.scoreSheet.put(ScoreType.FOURS, 0);
+        this.scoreSheet.put(ScoreType.FIVES, 0);
+        this.scoreSheet.put(ScoreType.SIXES, 0);
+        this.scoreSheet.put(ScoreType.BONUS, 0);
+
+        this.scoreSheet.put(ScoreType.LARGE_STRAIGHT, 0);
+        this.scoreSheet.put(ScoreType.SMALL_STRAIGHT, 0);
+        this.scoreSheet.put(ScoreType.FULL_HOUSE, 0);
+        this.scoreSheet.put(ScoreType.THREE_OF_A_KIND, 0);
+        this.scoreSheet.put(ScoreType.FOUR_OF_A_KIND, 0);
+        this.scoreSheet.put(ScoreType.CHANCE, 0);
+        this.scoreSheet.put(ScoreType.YAHTZEE, 0);
+    }
+
+    public void score(ScoreType scoreType, Dice[] dice) {
+        switch (scoreType) {
+        case ONES:
+        case TWOS:
+        case THREES:
+        case FOURS:
+        case FIVES:
+        case SIXES:
+            this.scoreUpperSection(scoreType, dice);
+        }
+    }
+
+    private void scoreUpperSection(ScoreType scoreType, Dice[] dice) {
+        int count = 0;
+        int section = scoreType.ordinal() + 1;
+
+        for (Dice d : dice) {
+            if (d.getValue() == section) {
+                count++;
+            }
+        }
+
+        this.scoreSheet.put(scoreType,
+                            this.scoreSheet.get(scoreType) + (section * count));
+    }
+
+    public int calculateScore() {
+        return this.scoreSheet.values()
+                              .stream()
+                              .mapToInt(Integer::intValue)
+                              .sum();
+    }
+
+    public HashMap<ScoreType, Integer> getScoreSheet() {
+        return this.scoreSheet;
+    }
+}
