32016f566cfecf5189bbb2060c5298f151f8a18d
Randy Taylor <tehgecKozzz@gmail.com>
2019-09-22 20:17:14 -0400

    refactor: Update Client to spawn threads for reading and writing to and from the server. This functions the exact same as before, except now it's threaded and allows for non-blocking operations between the client and server and vice versa at the same time

--- src/main/java/com/yahtzee/core/Client.java
+++ src/main/java/com/yahtzee/core/Client.java
@@ -4,11 +4,15 @@ package com.yahtzee.core;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.Socket;
 
 public class Client {
+    private Socket socket;
+
     public static void main(String[] args) {
         if (args.length != 2) {
             System.err.println("Specify an IP and port number");
@@ -18,25 +22,50 @@ public class Client {
         String host = args[0];
         int port = Integer.parseInt(args[1]);
 
-        try (Socket socket = new Socket(host, port);
-             PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));) {
-            BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+        Client client = new Client();
+        client.start(host, port);
+    }
 
-            while (true) {
-                String serverMessage = in.readLine();
-                if (serverMessage != null) {
-                    System.out.println(serverMessage);
-                }
-
-                String userInput = stdIn.readLine();
-                if (userInput != null) {
-                    out.println(userInput);
-                }
-            }
+    public void start(String host, int port) {
+        try {
+            this.socket = new Socket(host, port);
         } catch (IOException e) {
             System.out.println("Failed to connect to " + host);
             System.exit(-1);
+        }
+
+        try {
+            // Client-to-server.
+            new ClientThread(System.in, this.socket.getOutputStream()).start();
+            // Server-to-client.
+            new ClientThread(this.socket.getInputStream(), System.out).start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public class ClientThread extends Thread {
+        private final InputStream in;
+        private final OutputStream out;
+
+        public ClientThread(InputStream in, OutputStream out) {
+            this.in = in;
+            this.out = out;
+        }
+
+        @Override
+        public void run() {
+            try (BufferedReader in = new BufferedReader(new InputStreamReader(this.in));
+                 PrintWriter out = new PrintWriter(this.out, true)) {
+                while (true) {
+                    String message = in.readLine();
+                    if (message != null) {
+                        out.println(message);
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
