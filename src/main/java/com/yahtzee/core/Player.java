134f76672d4a2012986bb20044f69136954005bb
Randy Taylor <tehgecKozzz@gmail.com>
2019-09-24 22:54:38 -0400

    code: Add Player.toString() for networking

--- src/main/java/com/yahtzee/core/Player.java
+++ src/main/java/com/yahtzee/core/Player.java
@@ -2,6 +2,7 @@ package com.yahtzee.core;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -24,7 +25,7 @@ public class Player {
         }
 
         this.rollsCounter = 0;
-        this.turn = 0;
+        this.turn = 1;
 
         this.scoreSheet = new ScoreSheet();
     }
@@ -86,5 +87,42 @@ public class Player {
 
     public int getNumberOfRolls() {
         return this.rollsCounter;
+    }
+
+    @Override
+    public String toString() {
+        HashMap<ScoreType, Integer> scoreSheet = this.scoreSheet.getScoreSheet();
+
+        return
+            "---------------------------------------------------------------------------" +
+            "\n" +
+            "| Name: " + this.name.toString() +
+            " | Current Score: " + this.getScore() +
+            " | Current Round: " + this.turn +
+            " |\n" +
+            "--------------------------------------------------------------------------" +
+            "\n" +
+            "| (1) Ones: " + scoreSheet.get(ScoreType.ONES) +
+            " | (2) Twos: " + scoreSheet.get(ScoreType.TWOS) +
+            " | (3) Threes: " + scoreSheet.get(ScoreType.THREES) +
+            " | (4) Fours: " + scoreSheet.get(ScoreType.FOURS) +
+            " | (5) Fives: " + scoreSheet.get(ScoreType.FIVES) +
+            " | (6) Sixes: " + scoreSheet.get(ScoreType.SIXES) +
+            " | Bonus: " + scoreSheet.get(ScoreType.BONUS) +
+            " |\n" +
+            "--------------------------------------------------------------------------" +
+            "\n" +
+            "| (7) Large Straight: " + scoreSheet.get(ScoreType.LARGE_STRAIGHT) +
+            " | (8) Small Straight: " + scoreSheet.get(ScoreType.SMALL_STRAIGHT) +
+            " | (9) Full House: " + scoreSheet.get(ScoreType.FULL_HOUSE) +
+            " | (10) Three of a Kind: " + scoreSheet.get(ScoreType.THREE_OF_A_KIND) +
+            " |\n" +
+            "--------------------------------------------------------------------------" +
+            "\n" +
+            "| (11) Four of a Kind: " + scoreSheet.get(ScoreType.FOUR_OF_A_KIND) +
+            " | (12) Chance: " + scoreSheet.get(ScoreType.CHANCE) +
+            " | (13) Yahtzee!: " + scoreSheet.get(ScoreType.YAHTZEE) +
+            " |\n" +
+            "--------------------------------------------------------------------------";
     }
 }
